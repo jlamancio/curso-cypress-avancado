72d95db2e23e041ae10c0557a0489be8ee3cb270
Amancio <amancio.jluis@outlook.com>
2025-06-10 09:31:24 -0300

    JLA - refatoração do beforeEach - cy.intercept - passa a receber os argumentos como um objeto

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -1,13 +1,18 @@
 describe('Hacker Stories', () => {
   beforeEach(() => {
-    cy.intercept(
-      'GET',
-      '**/search?query=React&page=0'
-    ).as('getStories')
+    cy.intercept({
+      method: 'GET',
+      pathname: '**/search',
+      query: {
+        query: 'React',
+        page: '0'
+      }
+    }).as('getStories')
 
     cy.visit('/')
     cy.wait('@getStories')
-   
+
+
   })
 
   it('shows the footer', () => {
@@ -22,7 +27,7 @@ describe('Hacker Stories', () => {
     // and so, how can I assert on the data?
     // This is why this test is being skipped.
     // TODO: Find a way to test it out.
-    it.skip('shows the right data for all rendered stories', () => {})
+    it.skip('shows the right data for all rendered stories', () => { })
 
     it('shows 20 stories, then the next 20 after clicking "More"', () => {
       cy.get('.item').should('have.length', 20)
@@ -48,22 +53,22 @@ describe('Hacker Stories', () => {
     // This is why these tests are being skipped.
     // TODO: Find a way to test them out.
     context.skip('Order by', () => {
-      it('orders by title', () => {})
+      it('orders by title', () => { })
 
-      it('orders by author', () => {})
+      it('orders by author', () => { })
 
-      it('orders by comments', () => {})
+      it('orders by comments', () => { })
 
-      it('orders by points', () => {})
+      it('orders by points', () => { })
     })
 
     // Hrm, how would I simulate such errors?
     // Since I still don't know, the tests are being skipped.
     // TODO: Find a way to test them out.
     context.skip('Errors', () => {
-      it('shows "Something went wrong ..." in case of a server error', () => {})
+      it('shows "Something went wrong ..." in case of a server error', () => { })
 
-      it('shows "Something went wrong ..." in case of a network error', () => {})
+      it('shows "Something went wrong ..." in case of a network error', () => { })
     })
   })
 
