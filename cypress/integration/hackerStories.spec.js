19b487ef55a04b27c71032c2cb52e34f4e3f1274
Amancio <amancio.jluis@outlook.com>
2025-06-12 17:38:32 -0300

    JLA - Refatoração do código , testes front-end independentes do back-end (mocados)

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -15,9 +15,7 @@ describe('Hacker Stories', () => {
 
       cy.visit('/')
       cy.wait('@getStories')
-
     })
-
 
     it('shows 20 stories, then the next 20 after clicking "More"', () => {
       cy.intercept({
@@ -36,9 +34,7 @@ describe('Hacker Stories', () => {
       cy.get('.item').should('have.length', 40)
     })
 
-
     it('searches via the last searched term', () => {
-
       cy.intercept(
         'GET',
         `**search?query=${newTerm}&page=0`
@@ -63,139 +59,132 @@ describe('Hacker Stories', () => {
       cy.get(`button:contains(${newTerm})`)
         .should('be.visible')
     })
-
   })
 
+  context('Moking API', () => {
+    context('Footer and list of stories', () => {
+      beforeEach(() => {
+        cy.intercept(
+          'GET',
+          `**/search?query=${initialTerm}&page=0`,
+          { fixture: 'stories' }
+        ).as('getStories')
 
-  beforeEach(() => {
-    cy.intercept({
-      method: 'GET',
-      pathname: '**/search',
-      query: {
-        query: initialTerm,
-        page: '0'
-      }
-    }).as('getStories')
+        cy.visit('/')
+        cy.wait('@getStories')
+      })
 
-    cy.visit('/')
-    cy.wait('@getStories')
+      it('shows the footer', () => {
+        cy.get('footer')
+          .should('be.visible')
+          .and('contain', 'Icons made by Freepik from www.flaticon.com')
+      })
 
-  })
+      context('List of stories', () => {
+        it.skip('shows the right data for all rendered stories', () => { })
 
-  it('shows the footer', () => {
-    cy.get('footer')
-      .should('be.visible')
-      .and('contain', 'Icons made by Freepik from www.flaticon.com')
-  })
+        it('shows one less story after dimissing the first one', () => {
+          cy.get('.button-small')
+            .first()
+            .click()
 
-  context('List of stories', () => {
-
-    it.skip('shows the right data for all rendered stories', () => { })
-
-    it('shows only nineteen stories after dimissing the first story', () => {
-      cy.get('.button-small')
-        .first()
-        .click()
-
-      cy.get('.item').should('have.length', 19)
-    })
-
-    context.skip('Order by', () => {
-      it('orders by title', () => { })
-
-      it('orders by author', () => { })
-
-      it('orders by comments', () => { })
-
-      it('orders by points', () => { })
-    })
-
-
-  })
-
-  context('Search', () => {
-
-    beforeEach(() => {
-      cy.intercept(
-        'GET',
-        `**/search?query=${newTerm}&page=0`
-      ).as('getNewTermStories')
-
-      cy.get('#search')
-        .clear()
-    })
-
-    it('types and hits ENTER', () => {
-      cy.get('#search')
-        .type(`${newTerm}{enter}`)
-
-      cy.wait('@getNewTermStories')
-
-      cy.get('.item').should('have.length', 20)
-      cy.get('.item')
-        .first()
-        .should('contain', newTerm)
-      cy.get(`button:contains(${initialTerm})`)
-        .should('be.visible')
-    })
-
-    it('types and clicks the submit button', () => {
-      cy.get('#search')
-        .type(newTerm)
-      cy.contains('Submit')
-        .click()
-
-      cy.wait('@getNewTermStories')
-
-      cy.get('.item').should('have.length', 20)
-      cy.get('.item')
-        .first()
-        .should('contain', newTerm)
-      cy.get(`button:contains(${initialTerm})`)
-        .should('be.visible')
-    })
-
-    // Esse teste é apenas para mostrar alternativas possíveis com o Cypress, uma vez que a ação aqui não 
-    // pode ser realizada pelo usuário da ferramenta.
-
-    it('types and submits the form directly', () => {
-      cy.get('#search').type(newTerm)
-      cy.get('form').submit()
-
-      cy.wait('@getNewTermStories')
-
-      cy.get('.item').should('have.length', 20)
-
-    })
-
-
-    context('Last searches', () => {
-
-      Cypress._.times(1, () => {
-        it('shows a max of 5 buttons for the last searched terms', () => {
-          const faker = require('faker')
-
-          cy.intercept(
-            'GET',
-            '**/search**'
-          ).as('getRandomStories')
-
-          Cypress._.times(6, () => {
-            cy.get('#search')
-              .clear()
-              .type(`${faker.random.word()}{enter}`)
-            cy.wait('@getRandomStories')
-          })
-
-          cy.get('.last-searches button')
-            .should('have.length', 5)
+          cy.get('.item').should('have.length', 1)
         })
 
+        context.skip('Order by', () => {
+          it('orders by title', () => { })
+
+          it('orders by author', () => { })
+
+          it('orders by comments', () => { })
+
+          it('orders by points', () => { })
+        })
+      })
+    })
+
+    context('Search', () => {
+      beforeEach(() => {
+        cy.intercept(
+          'GET',
+          `**/search?query=${initialTerm}&page=0`,
+          { fixture: 'emptyStories' }
+        ).as('getEmptyStories')
+
+          cy.intercept(
+          'GET',
+          `**/search?query=${newTerm}&page=0`,
+          { fixture: 'stories' }
+        ).as('getStories')
+
+        cy.visit('/')
+        cy.wait('@getEmptyStories')
+
+        cy.get('#search')
+          .clear()
+      })
+
+      it.only('types and hits ENTER', () => {
+        cy.get('#search')
+          .type(`${newTerm}{enter}`)
+
+        cy.wait('@getStories')
+
+        cy.get('.item').should('have.length', 2)
+        cy.get(`button:contains(${initialTerm})`)
+          .should('be.visible')
+      })
+
+      it('types and clicks the submit button', () => {
+        cy.get('#search')
+          .type(newTerm)
+        cy.contains('Submit')
+          .click()
+
+        cy.wait('@getNewTermStories')
+
+        cy.get('.item').should('have.length', 2)
+        cy.get(`button:contains(${initialTerm})`)
+          .should('be.visible')
+      })
+
+      // Esse teste é apenas para mostrar alternativas possíveis com o Cypress, uma vez que a ação aqui não
+      // pode ser realizada pelo usuário da ferramenta.
+
+      it('types and submits the form directly', () => {
+        cy.get('#search').type(newTerm)
+        cy.get('form').submit()
+
+        cy.wait('@getNewTermStories')
+
+        cy.get('.item').should('have.length', 20)
+      })
+
+      context('Last searches', () => {
+        Cypress._.times(1, () => {
+          it('shows a max of 5 buttons for the last searched terms', () => {
+            const faker = require('faker')
+
+            cy.intercept(
+              'GET',
+              '**/search**'
+            ).as('getRandomStories')
+
+            Cypress._.times(6, () => {
+              cy.get('#search')
+                .clear()
+                .type(`${faker.random.word()}{enter}`)
+              cy.wait('@getRandomStories')
+            })
+
+            cy.get('.last-searches button')
+              .should('have.length', 5)
+          })
+        })
       })
     })
   })
 })
-
 
 context('Errors', () => {
   it('shows "Something went wrong ..." in case of a server error', () => {
@@ -209,7 +198,6 @@ context('Errors', () => {
     cy.wait('@getServerFailure')
 
     cy.get('p:contains(Something went wrong ...)')
-
   })
 
   it('shows "Something went wrong ..." in case of a network error', () => {
@@ -223,7 +211,5 @@ context('Errors', () => {
     cy.wait('@getNetworkFailure')
 
     cy.get('p:contains(Something went wrong ...)')
-
   })
-
 })
